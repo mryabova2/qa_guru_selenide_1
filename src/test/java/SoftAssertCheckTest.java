986c072c59d71c5abceb3f59ba8adbcb74af3bac
Maria Ryabova <mvryabova2@gmail.com>
2022-05-15 22:32:07 +0300

    Added Tests

---
+++ src/test/java/SoftAssertCheckTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertCheckTest {
+
+    @Test
+    void checkSoftAssertPage(){
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5"));
+    }
+
+    @Test
+    void replacingSquaresCheck(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").shouldHave(text("A"));
+    }
+}
